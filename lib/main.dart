65e4cf0d0cdccaa334e0283fbccb53ba77272e60
GioRLp <georgelcondo@gmail.com>
2026-06-07 00:07:51 -0400

    homescreen and blur

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -21,8 +23,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -30,77 +35,111 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool isEditing = false;
+  bool _isEditing = false;
+
+  void _editingChanged(bool value){
+    setState(() {
+      _isEditing = value;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context){
-    return Padding(
-      padding: const EdgeInsets.only(left: 16,right: 16,top: 48,bottom: 16),
-      child: Column(
-        children:[
-          SizedBox(
+    return Stack(
+      children: [
+
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children:[
+            SizedBox(
 
             width: double.infinity,
             child: Text(
               'Have a \ngreat \nday',
               style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
               textAlign: TextAlign.left,
+              ),
             ),
 
-          ),
+            ]
+          )
+        ),
 
-          SizedBox(height: 16),
 
-          GestureDetector(
-            onTap: (){
-              setState(() {
-                isEditing=true;
-              });
-            },
+        Positioned.fill(
+            child:BlurBlock(isVisible: _isEditing,
+            onTap: (){_editingChanged(false);},)
+        ),
+
+        Positioned(
+            top: 180,
+            left: 16,
+            right: 16,
+            child: FirstBlock(original: 'Are You ready', editingChanged: _editingChanged, isEditing:_isEditing),
+        ),
+
+      ]
+    );
+  }
+}
+
+
+class FirstBlock extends StatefulWidget{
+  final String original;
+  final bool isEditing;
+  final Function(bool) editingChanged;
+  const FirstBlock({super.key, required this.original, required this.isEditing,  required this.editingChanged});
+
+  @override
+  State<FirstBlock> createState() => _FirstBlockState();
+}
+
+class _FirstBlockState extends State<FirstBlock>{
+
+  @override
+  Widget build(BuildContext context){
+    return GestureDetector(
+        onTap: (){
+            widget.editingChanged(!widget.isEditing);
+        },
+        child: AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            height: widget.isEditing ? 400:200,
+            width: double.infinity,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.grey
+            ),
+            child: widget.isEditing ? TextField(autofocus: true) : Text(widget.original))
+    );
+  }
+}
+
+
+
+class BlurBlock extends StatelessWidget{
+  final bool isVisible;
+  final VoidCallback onTap;
+  const BlurBlock({super.key, required this.isVisible, required this.onTap});
+  
+  @override
+  Widget build(BuildContext context){
+    if(!isVisible) return SizedBox.shrink();
+    return ClipRect(
+      child: GestureDetector(
+        onTap: onTap,
+        behavior: HitTestBehavior.opaque,
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
             child: Container(
-                height: 200,
-                width: double.infinity,
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey,
-                ),
-                child:
-                Text(
-                  'Are you ready to share your plans, thoughts, maybe concerns?\nAdd a note or create a list.\nJust press into the gray block to start writing.',
-                  style: TextStyle(fontSize: 12),
-                )
+                color: Colors.grey.withOpacity(0.1)
             ),
-          ),
-
-
-          SizedBox(height: 16),
-
-          GestureDetector(
-            onTap: (){
-              setState(() {
-                isEditing=true;
-              });
-            },
-            child: Container(
-                height: 58,
-                width: double.infinity,
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey,
-                ),
-                child: Text(
-                  'would you like to remember and old note?',
-                  style: TextStyle(fontSize: 12),
-
-                )
-            ),
-          ),
-
-
-        ]
+          )
       )
     );
   }
 }
+
